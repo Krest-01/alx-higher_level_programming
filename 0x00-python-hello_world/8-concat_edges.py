75d9ce4b99bbb7a4b71b74306658d4e0a5252599
Ogboh Chinonso <Ogbohchinonso@gmail.com>
2023-09-04 17:12:35 -0700

    updated repo

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
+
+==================
+
+9-easter_egg.py
+
+#!/usr/bin/python3
+import this
+
+==================
